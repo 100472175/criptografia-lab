a11bece4a2709eef8eefc8c6f07da3004da42f44
SrS3R6IO <100472343@alumnos.uc3m.es>
2023-09-06 22:39:36 +0200

    minor changes in library page. Note: we have to keep (or search) for the current user.

--- pages/library.py
+++ pages/library.py
@@ -5,15 +5,15 @@ from streamlit_extras.switch_page_button import switch_page
 st.title("The Library")
 
 books = [
-    {"title": "The Great Gatsby", "genre": "Fiction","publication_year":1990 "length": 180},
-    {"title": "To Kill a Mockingbird", "genre": "Fiction", "length": 281},
-    {"title": "1984", "genre": "Science Fiction", "length": 328},
-    {"title": "Pride and Prejudice", "genre": "Romance", "length": 432},
-    {"title": "The Hobbit", "genre": "Fantasy", "length": 310},
-    {"title": "The Catcher in the Rye", "genre": "Fiction", "length": 277},
-    {"title": "The Lord of the Rings", "genre": "Fantasy", "length": 1178},
-    {"title": "Harry Potter and the Sorcerer's Stone", "genre": "Fantasy", "length": 309},
-    {"title": "The Da Vinci Code", "genre": "Mystery", "length": 454},
+    {"title": "The Great Gatsby", "genre": "Fiction","publication_year":1990, "length": 180, "reserved": "paco"},
+    {"title": "To Kill a Mockingbird", "genre": "Fiction", "length": 281, "reserved": False},
+    {"title": "1984", "genre": "Science Fiction", "length": 328, "reserved": False},
+    {"title": "Pride and Prejudice", "genre": "Romance", "length": 432, "reserved": False},
+    {"title": "The Hobbit", "genre": "Fantasy", "length": 310, "reserved": False},
+    {"title": "The Catcher in the Rye", "genre": "Fiction", "length": 277, "reserved": False},
+    {"title": "The Lord of the Rings", "genre": "Fantasy", "length": 1178, "reserved": False},
+    {"title": "Harry Potter and the Sorcerer's Stone", "genre": "Fantasy", "length": 309, "reserved": False},
+    {"title": "The Da Vinci Code", "genre": "Mystery", "length": 454, "reserved": False},
 ]
 
 
@@ -23,13 +23,15 @@ with col_books:
     with st.form("Book_reservation"):
         st.header("Books")
         st.write("Here you can see the books available in the library")
-        book_selection = st.selectbox("Select a book you want to make the reservation on:", books, format_func=lambda book: book["title"])
+        # mirar como hacer para que no muestre los que esten reservados
+        book_selection = st.selectbox("Select a book you want to make the reservation on:", books, format_func=lambda book: book["title"] if not book["reserved"] else book)
         submitted = st.form_submit_button("Select your book")
         if submitted:
             st.write(f"Your book is {book_selection['title']}, it is a {book_selection['genre']} book and it has {book_selection['length']} pages")
 
 
 with col_reservation:
-    st.header(f"{'User'} Reservations")
-    # Here, the reservations the user has already made are shown
-    #
+    st.header(f"{'Current'} Reservations")
+    # Here, the reservations the user has already made are shown (max 3)
+
+    #st.write("Your current reservations are:",books,format_func=lambda book: book["title"] if book["reserved"] == username else book)
