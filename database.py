c4d5251914fd5c2162a00f7484c82591078e43b3
100472175 <100472175@alumnos.uc3m.es>
2023-09-07 14:01:24 +0200

    New table with role added

--- database.py
+++ database.py
@@ -5,21 +5,21 @@ cur = con.cursor()
 #cur.execute("DROP TABLE USER")
 cur.execute("""
      CREATE TABLE USER (
-     username TEXT NOT NULL PRIMARY KEY,
-     password TEXT,
+     username VARCHAR(100) NOT NULL PRIMARY KEY,
+     password VARCHAR(100),
+     role VARCHAR(100),
      age INTEGER
      );
      """)
 
 cur.execute("""
     CREATE TABLE AVAILABLE_BOOKS(
-    BOOK_ID INT NOT NULL,
+    BOOK_ID INT NOT NULL PRIMARY KEY,
     BOOK_NAME VARCHAR(100) NOT NULL,
     AUTHOR_NAME VARCHAR(100) NOT NULL,
     PUBLICATION_YEAR INT NOT NULL,
     PAGE_COUNT INT NOT NULL,
-    RESERVED VARCHAR(100)
-    PRIMARY KEY(BOOK_ID)
+    AVAILABLE VARCHAR(100)
     );
 """)
 
