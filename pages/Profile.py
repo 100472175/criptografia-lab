ae025b50f62ed8037745e8baba3207f00af58984
100472175 <100472175@alumnos.uc3m.es>
2023-09-07 21:30:10 +0200

    Modificación reservas

--- pages/Profile.py
+++ pages/Profile.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
-from database_importer import delete_user, change_username, change_password
+from database_importer import delete_user
 import sqlite3 as sqllite
+from datetime import datetime
+import re
+from time import sleep
+
 
 def logged_in_profile():
     st.set_page_config(
@@ -14,44 +18,58 @@ def logged_in_profile():
     st.markdown("<h1 style='text-align: center;'>Profile</h1>", unsafe_allow_html=True)
     st.header("Here you can see your profile and make changes to it")
     st.write(f"You are logged as {username}")
-
-
     con = sqllite.connect("database.db")
     cur = con.cursor()
     cur.execute("SELECT * FROM USER WHERE username = ?", (username,))
     user = cur.fetchall()
     con.close()
-    user = user[0]
-    username_col, password_col, birthdate_col, identifier_col, role_col = st.columns(5)
-    with username_col:
-        new_username = st.text_input(f"Username: {user[0]}")
-        if st.button("Change username"):
-            try:
-                change_username(new_username,username)
-                st.session_state["username"] = new_username
-            except sqllite.IntegrityError:
-                st.error("Username already exists, please choose another one")
+    if user == []:
+        st.error("Wrong username or password")
+    else:
+        user = user[0]
+        id_valid = False
+        with st.form("update_profile"):
+            username_col, password_col, rol_col, identifier_col, birthdate_col = st.columns(5)
+            with username_col:
+                new_username = st.text_input("Username", value=user[0], key="username", disabled=True)
+            with password_col:
+                new_password = st.text_input("Password", value=user[1], key="password", type="password")
+            with rol_col:
+                st.text_input("Role", value=user[2], key="role", disabled=True)
+            with birthdate_col:
+                format = '%Y-%m-%d'
+                date = datetime.strptime(user[3], format)
+                new_date = st.date_input("Birthdate", key="birthdate", value=date)
+            with identifier_col:
+                new_id = st.text_input("DNI/NIF", value=user[4], key="identifier")
+            if st.form_submit_button("Update profile"):
+                st.write(new_username, new_password, new_date, new_id, username)
+                pattern = re.compile("^[0-9]{8}[A-Z]$")
+                id_valid = pattern.match(new_id)
+                if id_valid:
+                    con = sqllite.connect("database.db")
+                    cur = con.cursor()
+                    cur.execute("UPDATE USER SET  password = ?, birthdate = ?, id = ? WHERE username = ?",
+                                (new_password, new_date, new_id, username))
+                    con.commit()
+                    con.close()
+                    st.success("The details have been changed successfully")
+                else:
+                    st.error("The identifier is not valid, please enter a valid one")
 
 
 
-    with password_col:
-        new_password = st.text_input("Password:", type="password")
-        if st.button("Change password"):
-            try:
-                change_password(username, new_password)
-                st.session_state["username"] = new_username
-            except sqllite.IntegrityError:
-                st.error("Username already exists, please choose another one")
+            # Llamar a la funcion de cambio de cada una individualmente
 
 
-    if st.button("Log out",type= "secondary"):
-        st.session_state["username"] = ""
-        switch_page("Log In")
+        if st.button("Log out",type= "secondary"):
+            st.session_state["username"] = ""
+            switch_page("Log In")
 
-    if st.button("Delete Account", type="primary"):
-        delete_user(username)
-        st.session_state["username"] = ""
-        switch_page("Log In")
+        if st.button("Delete Account", type="primary"):
+            delete_user(username)
+            st.session_state["username"] = ""
+            switch_page("Log In")
 
 
 def draw_not_logged():
